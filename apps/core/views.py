faef284ad0fa59121d03d627189e4a4cebee250a
RifatArefin32 <arefinrifat786@gmail.com>
2025-07-01 07:54:54 +0600

    Serializer subclasses

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,9 @@
+from django.db.models import Max
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from apps.core.models import Product, Order
-from apps.core.serializers import ProductSerializer, OrderSerializer
+from apps.core.serializers import ProductSerializer, OrderSerializer, ProductsInfoSerializer
 
 # Show all products
 @api_view(['GET'])
@@ -18,11 +19,20 @@ def product_details(request, pk):
     serializer = ProductSerializer(product)
     return Response(serializer.data)
 
-
-
 # Show all orders
 @api_view(['GET'])
 def order_list(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+# Show all product info
+@api_view(['GET'])
+def products_info(requeest):
+    products = Product.objects.all()
+    serializer = ProductsInfoSerializer({
+        'products': products,
+        'count': len(products),
+        'max_price': products.aggregate(max_price=Max('price'))['max_price']
+    })
     return Response(serializer.data)
